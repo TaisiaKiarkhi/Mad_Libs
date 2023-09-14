2e15dc0bc8faf0de6d65bac5b350f4c7d3804b07
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2023-09-14 08:55:54 +0200

    Add project files.

---
+++ Mad_Libs/Mad_Libs.py
@@ -0,0 +1,15 @@
+print ("Hi! This is a Mad Lib! Type in your words!")
+
+_verb = input("Enter a verb: ")
+_noun = input("Enter a noun: ")
+adverb = input("Enter an adverb: ")
+_verb_pt = input("Enter a verb in past tense: ")
+_plural_noun = input("Enter a plural noun: ")
+_plural_noun2 = input("Enter a plural noun: ")
+_adjective = input("Enter an adjective: ")
+_plural_noun3 = input("Enter a plural noun: ")
+_verb_ing = input("Enter a verb ending in -ing: ")
+
+print("O say can you " + _verb + " by the dawns early " + _noun + ".")
+print("What so " + adverb+ " we " + _verb_pt + " at the twilight's last gleaming, Whose broad " + _plural_noun + 
+      " and bright " + _plural_noun2 + " through the " + _adjective + " fight, O'er the "+ _plural_noun3+ " we watched, were so gallantly " +_verb_ing+"?")
